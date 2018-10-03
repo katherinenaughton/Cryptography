843e9138e4449225801162a007644ac6cb771514
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-10-03 11:46:57 -0400

    Updated from Brython Server: 10/3/2018 11:46:56 AM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Katie Naughton
+Credit: I worked alone. 
 
 Assignment:
 
@@ -10,3 +10,8 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+func=float(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+for i in "abcdefghijklmnopqrstuvwxyz":
+    if i=! e or d or q:
+        print("Did not understand command, try again.")
