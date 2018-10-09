71380fd095c9a193c7d16c65a56ad8cd87c5f91b
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-10-09 11:38:42 -0400

    Updated from Brython Server: 10/9/2018 11:38:42 AM

--- cryptography.py
+++ cryptography.py
@@ -9,16 +9,13 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-func=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-while func !='q':
-    print(c)
-    if c=='q':
-        print('Goodbye!')
-    elif c== 'e' or c=='d':
+f=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+while f !='q':
+    if c== 'e' or c=='d':
         m=input("Message: ")
         k=input("Key: ")
         print(m)
         print(k)
-        
-    else: 
-        print("Did not understand command, try again.")
+    else: print("Did not understand command, try again.")
+if f=='q':
+    print('Goodbye!')
